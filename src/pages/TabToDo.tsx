00a64d48b40d3f20858234bd28bdddef249d9bbd
Irina Sorokina <irasoro402@yandex.ru>
2022-07-16 04:56:35 +0400

    Added editing and deleting tasks

--- src/pages/TabToDo.tsx
+++ src/pages/TabToDo.tsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import {
   IonButtons,
   IonButton,
@@ -13,59 +13,183 @@ import {
   IonLabel,
   IonInput,
   IonDatetime,
+  IonIcon,
+  useIonAlert,
+  IonReorder,
+  IonSelect, IonSelectOption,
 } from '@ionic/react';
+import { create, close, add, trash } from 'ionicons/icons';
+
 import './TabToDo.css';
 
+interface PropsListToDo {
+  listToDo: string[];
+  setListToDo: (newListCards: string[]) => void;
+}
+
+interface PropsListToDo {
+  listToDo: string[];
+  setListToDo: (newListCards: string[]) => void;
+}
+
+const AddButton = (props: PropsListToDo) => {
+  const [presentAlert] = useIonAlert();
+
+  return (
+    <IonButton slot="end" onClick={() => presentAlert({
+      header: 'Task enter',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'OK',
+          role: 'confirm'
+        }
+      ],
+      inputs: [
+        {
+          type: 'textarea',
+          placeholder: 'Task'
+        }
+      ],
+      onDidDismiss: (e: CustomEvent) => {
+        if (e.detail.role === 'confirm') {
+          props.listToDo.push(e.detail.role);
+          props.setListToDo([...props.listToDo]);
+        }
+      }
+    })}>
+      <IonIcon slot="icon-only" icon={add} />
+    </IonButton>
+  )
+}
+
+const ToDoList = (props: PropsListToDo) => {
+  const [presentAlertEdit] = useIonAlert();
+  const [presentAlertDelete] = useIonAlert();
+
+  const toDoList = props.listToDo.map((value, index) => {
+    return (
+      <IonItem key={index} >
+        <IonReorder slot="start" />
+        <IonLabel>
+          {value}
+        </IonLabel>
+        <IonSelect
+          interface="popover"
+          value=" "
+          onIonChange={(e) => {
+            if (e.detail.value === "edit") {
+              presentAlertEdit({
+                header: 'Task editing',
+                buttons: ['OK'],
+                inputs: [
+                  {
+                    type: 'textarea',
+                    value: value,
+                  }
+                ],
+                onDidDismiss: (e: CustomEvent) => {
+                  props.listToDo[index] = e.detail.data.values[0];
+                  props.setListToDo([...props.listToDo]);
+                }
+              })
+            }
+            if (e.detail.value === "delete") {
+              presentAlertDelete({
+                header: "Delete task?",
+                buttons: [
+                  {
+                    text: 'Cancel',
+                    role: 'cancel'
+                  },
+                  {
+                    text: 'OK',
+                    role: 'confirm',
+                  }
+                ],
+                onDidDismiss: (e: CustomEvent) => {
+                  if (e.detail.role === 'confirm') {
+                    props.listToDo.splice(index, 1);
+                    props.setListToDo([...props.listToDo]);
+                  }
+                }
+              })
+            }
+          }}>
+          <IonSelectOption value="edit">Edit</IonSelectOption>
+          <IonSelectOption value="delete">Delete</IonSelectOption>
+        </IonSelect>
+        {/* <IonButton
+          color="light"
+          size="small"          
+          onClick={() => presentAlertEdit({
+            header: 'Task editing',
+            buttons: ['OK'],
+            inputs: [
+              {
+                type: 'textarea',
+                value: value,
+              }
+            ],
+            onDidDismiss: (e: CustomEvent) => {
+              props.listToDo[index] = e.detail.data.values[0];
+              props.setListToDo([...props.listToDo]);
+            }
+          })}>
+          <IonIcon slot="icon-only" icon={create} />
+        </IonButton>
+        <IonButton
+          color="light"
+          size="small"
+          onClick={() => presentAlertDelete({
+            header: "Delete task?",
+            buttons: [
+              {
+                text: 'Cancel',
+                role: 'cancel'
+              },
+              {
+                text: 'OK',
+                role: 'confirm',
+              }
+            ],
+            onDidDismiss: (e: CustomEvent) => {
+              if (e.detail.role === 'confirm') {
+                props.listToDo.splice(index, 1);
+                props.setListToDo([...props.listToDo]);
+              }
+            }
+          })}>
+          <IonIcon slot="icon-only" icon={trash} />
+        </IonButton> */}
+      </IonItem>
+    );
+  })
+
+  return (
+    <IonList>
+      {toDoList}
+    </IonList>
+  );
+}
+
 function TabToDo() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [listToDo, setListToDo] = useState(["Task1", "Task2", "Task3"]);
+  console.log("listToDo = ", listToDo);
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          <AddButton listToDo={listToDo} setListToDo={setListToDo} />
           <IonTitle>ToDo</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-
-        <IonButton expand="block" onClick={() => setIsOpen(true)}>
-          Add
-        </IonButton>
-        <IonModal isOpen={isOpen}>
-        <IonHeader>
-            <IonToolbar>
-
-              <IonButtons slot="end">
-                <IonButton onClick={() => setIsOpen(false)}>Confirm</IonButton>
-              </IonButtons>
-
-              <IonTitle>Adding</IonTitle>
-
-              <IonButtons slot="start">
-                <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
-              </IonButtons>
-
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">
-
-            <IonList>
-
-              <IonItem>
-                <IonLabel position="floating">ToDo</IonLabel>
-                <IonInput></IonInput>
-              </IonItem>
-
-              <IonItem>
-                <IonDatetime color="blue" ></IonDatetime>
-              </IonItem>
-
-            </IonList>
-
-            </IonContent>
-        </IonModal>
-
-
+        <ToDoList listToDo={listToDo} setListToDo={setListToDo} />
       </IonContent>
     </IonPage>
   );
