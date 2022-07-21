6d6c4c0c4a30a8eead229249dd28ae6ad108b5b3
Irina Sorokina <irasoro402@yandex.ru>
2022-07-21 17:07:07 +0400

    Added adding list

--- src/pages/TabPurchases.tsx
+++ src/pages/TabPurchases.tsx
@@ -8,27 +8,23 @@ import {
   IonTitle,
   IonPage,
   IonItem,
-  IonList,
   IonLabel,
   IonInput,
   IonCard,
-  IonCardContent,
-  useIonModal,
   IonIcon,
   useIonAlert,
   IonModal,
   IonCheckbox,
-  IonReorder, IonAvatar, IonImg, useIonActionSheet
+  IonReorder, 
 } from '@ionic/react';
 
 import { create, close, add } from 'ionicons/icons';
-import { OverlayEventDetail } from '@ionic/core/components';
 import './TabPurchases.css';
-import { Interface } from 'readline';
 
 interface ElementObject {
   name: string;
-  isDone: boolean
+  isDone: boolean;
+  count: number;
 }
 
 interface ListObject {
@@ -50,7 +46,10 @@ const ListElements = (props: PropsList) => {
 
       return (
         <IonItem key={j} >
-          <IonLabel>{valueEl.name}</IonLabel>
+          <IonLabel>
+            <h2>{valueEl.name}</h2>
+            <p>count: {valueEl.count}</p>
+          </IonLabel>
           <IonCheckbox
             slot="start"
             checked={valueEl.isDone}
@@ -116,8 +115,6 @@ const ListElements = (props: PropsList) => {
 }
 
 
-
-
 const AddingModal = (props: PropsList) => {
   const modal = useRef<HTMLIonModalElement>(null);
   const page = useRef(null);
@@ -142,6 +139,18 @@ const AddingModal = (props: PropsList) => {
     }
   );
 
+  const Elements = newList.elements.map((value, index) => {
+    return (
+      <IonItem key={index}>
+        <IonReorder slot="start" />
+        <IonLabel>
+          <h2>{value.name}</h2>
+          <p>count: {value.count}</p>
+        </IonLabel>
+      </IonItem>
+    );
+  });
+
   return (
     <IonModal ref={modal} trigger="add-modal" presentingElement={presentingElement!}>
       <IonHeader>
@@ -150,7 +159,19 @@ const AddingModal = (props: PropsList) => {
             <IonButton onClick={() => dismiss()}>Close</IonButton>
           </IonButtons>
           <IonButtons slot="end">
-            <IonButton>Config</IonButton>
+            <IonButton onClick={ () => {
+                newList.title = String(inputRef.current?.value);
+                props.listElem.push(newList);
+                props.setList([...props.listElem]);
+                //TODO:do a normal cleanup of the object
+                setNewList({
+                  title: "",
+                  elements: []
+                });
+                dismiss();
+              }
+            }>
+              Confirm</IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
@@ -184,16 +205,14 @@ const AddingModal = (props: PropsList) => {
           ],
           onDidDismiss: (e: CustomEvent) => {
             if (e.detail.role === 'confirm') {
-              newList.elements.push({name: e.detail.data.values[0], isDone: false});
-              setNewList(newList);
-              console.log("list = ", newList);
-              // console.log("1 = ", e.detail.data.values[0]);
-              // console.log("2 = ", e.detail.data.values[1]);
+              newList.elements.push({ name: e.detail.data.values[0], isDone: false, count: e.detail.data.values[1] });
+              //TODO:change clone object
+              setNewList(Object.assign(Object.create(newList), newList));
             }
           }
-          
-        })}>Add element</IonButton>
 
+        })}>Add element</IonButton>
+        {Elements}
       </IonContent>
     </IonModal>
   );
@@ -208,14 +227,17 @@ function TabPurchases() {
         {
           name: "ELEM 1",
           isDone: true,
+          count: 5,
         },
         {
           name: "Elem 2",
           isDone: false,
+          count: 5678,
         },
         {
           name: "Elem 3",
           isDone: true,
+          count: 8788,
         }
       ]
     },
@@ -225,14 +247,17 @@ function TabPurchases() {
         {
           name: "ELEM 1",
           isDone: true,
+          count: 234,
         },
         {
           name: "Elem 2",
           isDone: false,
+          count: 34,
         },
         {
           name: "Elem 3",
           isDone: true,
+          count: 345432,
         }
       ]
     },
